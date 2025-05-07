c9313e33504d1b794311717195f149a7abb7cd72
OskrGa08 <bunbueygasp@gmail.com>
2025-05-07 11:25:19 -0600

    base de datos vacia

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,21 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
-
 
 db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = 'main.login'  # Redirigir a esta vista si no está autenticado
-
-@login_manager.user_loader
-def load_user(user_id):
-    from app.models import Usuario
-    return Usuario.query.get(int(user_id))
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'e5b0c3f8a9d4e7f1b2c3d4e5f6a7b8c9'
-    login_manager.init_app(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies_reviews.db'
+    app.secret_key = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['DEBUG'] = True  # Habilitar el modo de depuración
 
     db.init_app(app)
-    migrate = Migrate(app, db)
+    migrate.init_app(app, db)
 
     from .routes import main
     app.register_blueprint(main)
 
     return app
-
