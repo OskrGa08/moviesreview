5bc56899a67ed78ec0f61ba48af59ed9d001f653
OskrGa08 <bunbueygasp@gmail.com>
2025-05-07 12:04:01 -0600

    Definir relación entre Resena y Usuario

--- app/models.py
+++ app/models.py
@@ -2,15 +2,18 @@ from app import db
 from flask_login import UserMixin
 
 class Usuario(UserMixin, db.Model):
+    __tablename__ = 'usuario'
     id_usuario = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+
     def get_id(self):
         return str(self.id_usuario)
     
 class Pelicula(db.Model):
+    __tablename__ = 'pelicula'
     id_pelicula = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(200))
     director = db.Column(db.String(150))
@@ -20,9 +23,8 @@ class Pelicula(db.Model):
     resenas = db.relationship('Resena', backref='pelicula', lazy=True)
 
 class Resena(db.Model):
+    __tablename__ = 'resena'
     id_resena = db.Column(db.Integer, primary_key=True)
-    comentario = db.Column(db.Text)
-    puntuacion = db.Column(db.Integer)
-    fecha_resena = db.Column(db.DateTime, default=db.func.current_timestamp())
+    contenido = db.Column(db.Text, nullable=False)
     id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'), nullable=False)
-    id_pelicula = db.Column(db.Integer, db.ForeignKey('pelicula.id_pelicula'), nullable=False)
+    usuario = db.relationship('Usuario', backref='resenas')
